f35fc2e5a79faab50079987cb53bcf72cd598d2f
lengsovandara168 <lengsovandara7@gmail.com>
2026-02-25 01:44:14 +0700

    remove proxy.ts

--- src/proxy.ts
+++
@@ -1,9 +0,0 @@
-import createMiddleware from "next-intl/middleware";
-import { routing } from "./i18n/routing";
-
-export default createMiddleware(routing);
-
-export const config = {
-  // Updated matcher to only include en and km
-  matcher: ["/", "/(en|km)/:path*", "/((?!api|_next|_vercel|.*\\..*).*)"],
-};
